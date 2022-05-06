7e4cd9308fd4d892dd0914c6d3c66b0b1ba5e5de
Larissa-Nahan <larissanahan@gmail.com>
2022-05-05 21:02:39 -0300

    Segunda resolução

--- js/index.js
+++ js/index.js
@@ -7,29 +7,62 @@ const $buttonsOperator = document.querySelectorAll(
 const $inputGetNumbers = document.querySelector(".calculator__display-input");
 const $result = document.querySelector(".calculator__display-result");
 const $buttonResult = document.querySelector(".calculator--button--result");
+const $clean = document.querySelector(".clean");
 
 let calculate = "";
+let n1 = null;
+let n2 = null;
+let operation = null;
 
 $buttonsNumbers.forEach(function ($button) {
   $button.addEventListener("click", function () {
-    // alert(`clicado: ${$button.textContent}`);
     calculate = calculate + $button.textContent;
     $inputGetNumbers.value = calculate.replaceAll(" ", "");
+    if (n1 === null) {
+      n1 = Number($button.textContent);
+    } else {
+      n2 = Number($button.textContent);
+    }
   });
 });
 
 $buttonsOperator.forEach(function ($button) {
   $button.addEventListener("click", function () {
-    // alert(`clicado: ${$button.textContent}`);
     calculate = calculate + $button.textContent;
     $inputGetNumbers.value = calculate.replaceAll(" ", "");
+    if (operation === null) {
+      operation = $button.textContent;
+    }
   });
 });
 
 $buttonResult.addEventListener("click", function () {
-  calculate = calculate.replaceAll("x", "*");
-  calculate = calculate.replaceAll("÷", "/");
-  $result.textContent = eval(calculate);
-
+  // calculate = calculate.replaceAll("x", "*");
+  // calculate = calculate.replaceAll("÷", "/");
+  // $result.textContent = eval(calculate);
+  let result = null;
+  if (operation.includes("+")) {
+    result = n1 + n2;
+  }
+  if (operation.includes("-")) {
+    result = n1 - n2;
+  }
+  if (operation.includes("x")) {
+    result = n1 * n2;
+  }
+  if (operation.includes("÷")) {
+    result = n1 / n2;
+  }
+  if (operation.includes("%")) {
+    result = n1 % n2;
+  }
+  $result.textContent = result;
 });
 
+$clean.addEventListener("click", function () {
+  $inputGetNumbers.value = "";
+  $result.textContent = "";
+  calculate = "";
+  n1 = null;
+  n2 = null;
+});
